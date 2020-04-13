42f772e29785e9a6c4a8a0fcd2b6bfe98a29e527
Adrian <mail@adrianapan.com>
2020-04-13 09:10:26 +0100

    GA and mobile tweaks

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,17 @@
 import React from "react"
+import ReactGA from "react-ga"
 
 import "./App.scss"
 import Welcome from "../Welcome"
 
 function App() {
+  ReactGA.initialize("UA-69829176-3")
+  ReactGA.pageview(window.location.pathname + window.location.search)
+
   return (
-  <div className="app">
-    <Welcome />
-  </div>
+    <div className="app">
+      <Welcome />
+    </div>
   )
 }
 
--- src/utils/projectsData.js
+++ src/utils/projectsData.js
@@ -12,32 +12,5 @@ export default [
 			"html5",
 			"css3"
     ]
-  },
-  {
-    id: 2,
-    name: "Lorem ipsum",
-    url: "https://x.com",
-    tech: [
-			"javascript",
-			"nodejs",
-			"express",
-			"postgresql",
-			"bootstrap",
-			"html5",
-			"css3"
-    ]
-  },
-  {
-    id: 99,
-    name: "Portfolio",
-    url: "/",
-    tech: [
-			"react",
-			"javascript",
-			"nodejs",
-			"html5",
-			"css3",
-			"semantic"
-    ] 
-	}
+  }
 ]
